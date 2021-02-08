aba753b1dc7c353a5122d481aa0ebfbce34d3b3c
Tim Fernandez <tim.fernandezhart@gmail.com>
2021-02-08 09:25:06 +0000

    Threading branch merged.

---
+++ tests/test_thermometer.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestThermometer(TestCase):
+    def test_get_temperature(self):
+        self.fail()
+
+    def test__update_temperature(self):
+        self.fail()
